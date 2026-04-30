0212de3031a334d5c4fea0ce93f4d2dc08454c66
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-04-30 15:28:22 +0545

    feat: add basic practice of js

---
+++ JavaScript/first.mjs
@@ -0,0 +1,41 @@
+console.log("Wassupppppp") // string
+
+console.log(1) // number
+console.log(true) // boolean
+console.log(true) // boolean
+console.log(true) // boolean
+console.log(true) // boolean
+console.log(true) // boolean
+console.log(true) // boolean
+// console.log(true) // boolean --> Single line comment
+console.log(true) // boolean
+console.log(true) // boolean --> Half Line Comment
+console.log(true) // boolean
+console.log(true) // boolean
+/* console.log(true)
+console.log(true) 
+console.log(true) 
+console.log(true) */
+// console.log(true) 
+
+/* number 1, 2, 4.2, 4,4
+string "a" "true"
+Boolean true false */
+
+// Variables
+let name = "Ram"
+let ward = 1
+let addr = "Ktm"
+let hasCar = false
+
+// calling variable
+console.log(name, ward, addr, hasCar)
+
+//changing variable
+name = "Hari"
+console.log(name)
+
+const country = "Nepal"
+console.log(country)
+
+// country = "India" --> Const can't be changed
