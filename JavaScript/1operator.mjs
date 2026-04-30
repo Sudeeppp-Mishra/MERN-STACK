aabdc7f760dd97b12b7873f13b330be00d4d6598
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-04-30 15:28:42 +0545

    feat: add operators in js examples

---
+++ JavaScript/1operator.mjs
@@ -0,0 +1,23 @@
+// Arithmetic Operator 
+console.log(1+1)
+console.log(1-1)
+console.log(1*1)
+console.log(1/1)
+console.log(3%2)
+
+// Assignment Operator '='
+let a = 10 // assigns value 10 to variable a
+
+// Comparison Operator '<', '>', '<=', '>=', '==', '!='
+console.log(3>2) // true
+console.log(3<2) // false
+console.log(3==2) // false -> for comparing values only and auto typecasts
+console.log(3!=2) // true
+console.log(3>=3) // true
+console.log(3<=2) // false
+console.log(3===3) // true -> for comparing value and also type strictly
+
+// Logical Operator '&&', '||', '!'
+console.log(!true) // false
+console.log(true && false && true && true) // false
+console.log(true || false || false) // true
