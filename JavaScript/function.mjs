7effac44742ee15ad4e88673c91a130f6bf32976
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-05-05 12:18:39 +0545

    feat: add undefined and default values examples

--- JavaScript/function.mjs
+++ JavaScript/function.mjs
@@ -15,3 +15,19 @@ let studentInfo = (name, roll, program) =>{
 
 studentInfo("Ram Sha", 23, "BE-Computer")
 studentInfo("Hari Jha", 25, "Bsc.CSIT")
+
+/*Undefined Values*/
+let studentInformation = (name, roll, program)=>{
+    console.log(name, roll, program)
+};
+
+studentInformation("Ram"); // Undefined values for roll and program
+
+/* Default Values */
+let stdInfo = (name, roll, program="BE-Computer")=>{
+    console.log(name, roll, program)
+};
+
+stdInfo("Hari"); // One undefined and another is default
+stdInfo("Ram", 18); // Default
+stdInfo("Shyam", 19, "BE-Civil")
