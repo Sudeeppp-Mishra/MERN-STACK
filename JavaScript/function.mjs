ba46f13c876608db9c25d679f79a1a7e758dbbd9
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-05-05 12:00:33 +0545

    feat: add simple arrow function exmaple

---
+++ JavaScript/function.mjs
@@ -0,0 +1,10 @@
+// First Class Function i.e., a variable storing function
+let func1 = () => { /* Arrow Function Definition */
+    console.log("Function")
+};
+
+console.log("idk")
+func1();
+func1; // Not a right way to call function
+
+
