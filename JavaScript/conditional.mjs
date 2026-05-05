fc0b1b0d09fcbdeeb3218122362cbfa6f2cd94b6
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-05-05 11:33:05 +0545

    fix typo: add another example of conditional statement

--- JavaScript/conditional.mjs
+++ JavaScript/conditional.mjs
@@ -4,3 +4,18 @@ if(age>=18)
     console.log("Can vote")
 else
     console.log("Cannot vote")
+
+/*
+male => he
+female => she
+other => they
+*/
+
+let gender = "male";
+
+if(gender==="male")
+    console.log("HE");
+else if(gender==="female")
+    console.log("SHE")
+else
+    console.log("THEY")
