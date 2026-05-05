588a35fae87a697cdf2067cfc1ef016631e9ad76
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-05-05 12:05:00 +0545

    feat: add passing values in function example

--- JavaScript/function.mjs
+++ JavaScript/function.mjs
@@ -8,3 +8,10 @@ func1();
 func1; // Not a right way to call function
 
 
+/* Passing values through function */
+let studentInfo = (name, roll, program) =>{
+    console.log(name, roll, program)
+};
+
+studentInfo("Ram Sha", 23, "BE-Computer")
+studentInfo("Hari Jha", 25, "Bsc.CSIT")
