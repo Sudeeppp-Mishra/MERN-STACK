3bd115964d75a7026ff68f9fda7f0a3e640b71e5
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-05-07 16:19:44 +0545

    feat: add backtick concept

---
+++ JavaScript/12backtTick.mjs
@@ -0,0 +1,10 @@
+// String can be wrapped into either "str" or 'str' or `str`
+console.log("first");
+console.log('first');
+console.log(`first`);
+
+let name = "Ram";
+let surname = "Thapa";
+
+let info = `My name is ${name} ${surname}`; // backticks can help put placeholders for variables
+console.log(info)
