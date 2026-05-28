c351bde5006965209203bde9462d226e154d4205
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-05-28 12:17:45 +0545

    feat: make route and add to index.js

---
+++ Backend/index.js
@@ -0,0 +1,47 @@
+/*
+    make express application
+    attach port to it
+*/
+
+import express from "express";
+import firstRoute from "./src/routes/firstRoute.js";
+let app = express();
+
+app.listen(8000, () => {
+    console.log("Application is listening at port 8000!");
+});
+
+/*
+
+DD operations (CRUD)
+
+Create -> post
+Read -> get
+Update -> patch
+Delete -> delete
+
+*/
+
+/*
+app.post("/", (req, res, next)=>{ // "/" means "localhost:port" i.e., "localhost:8000"
+    console.log("Post Method");
+});
+
+app.get("/", (req, res, next)=>{
+    console.log("Get Method");
+});
+
+app.patch("/", (req, res, next)=>{
+    console.log("Patch Method");
+});
+
+app.delete("/", (req, res, next)=>{ // these are basically apis
+    console.log("Delete Method");
+});
+
+app.get("/test/test1", (req, res, next)=>{
+    console.log("Testing...");
+});
+*/
+
+app.use(firstRoute);
---
+++ Backend/src/routes/firstRoute.js
@@ -0,0 +1,8 @@
+// make route 
+// use that route at index.js
+
+import { Router } from "express";
+
+let firstRoute = Router();
+
+export default firstRoute;
