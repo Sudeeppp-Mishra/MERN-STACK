bbbe5a95939c9d48129156d3a54fae817b8ed40d
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-06-04 12:15:07 +0545

    feat: add other codes to add data to mongoDB

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,18 +1,34 @@
-import {Router} from "express";
+import { Router } from "express";
+import User from "../schema/userSchema.js";
 
 let userRoutes = Router();
 
 userRoutes
-.route("/")
-.post((req, res, next)=>{
-    res.json("user created successfully!");
-})
-.get((req, res, next)=>{});
+  .route("/") // localhost:8000/user
+  .post(async (req, res, next) => {
+    // put async here for putting in db
+    //res.json("user created successfully!");
+    /*
+        send data from postman
+        get data sent by postman
+        store data in User table
+    */
+
+    // console.log(req.body) // get data in console
+    let result = await User.create(req.body); // put await in front of table of db
+
+    res.json({
+      success: true,
+      message: "Data added to User table successfully!",
+      result: result,
+    });
+  })
+  .get((req, res, next) => {});
 
 userRoutes
-.route("/:id")
-.get((req, res, next)=>{})
-.patch((req, res, next)=>{})
-.delete((req, res, next)=>{})
+  .route("/:id")
+  .get((req, res, next) => {})
+  .patch((req, res, next) => {})
+  .delete((req, res, next) => {});
 
 export default userRoutes;
