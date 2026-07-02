0a73d22d81d39b230de3d9601c4a77f65b2692f1
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-07-02 17:48:19 +0545

    feat: add more exercises of react

--- React/vite-project/src/MyApp.jsx
+++ React/vite-project/src/MyApp.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LearnUseState from './LearnUseState.jsx'
 import CosmosLink from './CosmosLink.jsx'
+import CosmosRoute from './CosmosRoute.jsx'
 
 const MyApp = () => {
   return (
     <div>
     {/* <LearnUseState></LearnUseState> */}
       <CosmosLink></CosmosLink>
+      <CosmosRoute></CosmosRoute>
     </div>
   )
 }
--- React/vite-project/src/main.jsx
+++ React/vite-project/src/main.jsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import "./global.css"
-import MyApp from "./MyApp.jsx"
-import {BrowserRouter} from "react-router-dom"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "./global.css";
+import MyApp from "./MyApp.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     <BrowserRouter>
+      {/* we need to wrapt the main app by BrowserRouter if we are using react-router-dom for changing the  */}
       <MyApp></MyApp>
     </BrowserRouter>
   </StrictMode>,
-)
+);
