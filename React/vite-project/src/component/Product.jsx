fef19554b823c541a98d74e273f94e086ea54223
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-07-03 12:59:50 +0545

    chore: add comment for dynamic params

--- React/vite-project/src/component/Product.jsx
+++ React/vite-project/src/component/Product.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 
 const Product = () => {
     let params = useParams()
-    console.log(params)
+    console.log(params) // Object: id1: ..., id2: ...
   return (
     <div>Product Page</div>
   )
